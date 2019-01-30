9c6a269bd203518b7f4c515cf8b4a20871e7fc9e
Yauheni Tsiarletski <Yauheni_Tsiarletski@epam.com>
2019-01-30 10:40:15 +0300

    update

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-const mongoDB = require(`./mongoDbMethods.js`);
-const sheet = require(`./sheets.js`);
+const mongo = require(`./mongoDbMethods.js`);
+const sheets = require(`./sheets.js`);
 const auth = require(`./authorization.js`);
-
 const methods = process.argv[2];
 
 async function mongoDbMethods () {
   switch (methods) {
+    case `findDocument`:
+      mongo.dropCollection();
+      break;
+    case `deleteDocument`:
+      mongo.dropCollection();
+      break;
+    case `drop`:
+      mongo.dropCollection();
+      break;
     case `insertMany`:
-      mongoDB.getDataFromJson();
+      mongo.getDataFromJson();
       break;
     case `writeToSheets`:
-      let data = await mongoDB.getData();
+      let data = await mongo.getData();
       const client = auth.authorize();
-      sheet.writeToSheet(client, data);
+      sheets.writeToSheet(client, data);
       break;
-    default: console.log(`Incorrect method`);
+    default: console.log(`Incorrect method`)
       client.close();
       break;
   }
--- mongoDbMethods.js
+++ mongoDbMethods.js
@@ -2,31 +2,66 @@ const MongoClient = require(`mongodb`).MongoClient;
 const users = require(`./models/users.json`);
 const cars = require(`./models/cars.json`);
 
-async function getDataFromJson () {
+async function findDocument() {
   const client = await getClient();
   const db = await getDB(client);
+  const query = { address: /^S/ };
+  db.collection(`users`).find(query).toArray((err, result) => {
+    if (err) throw err;
+    console.log(result);
+  });
+  client.close();
+};
 
+async function deleteDocument() {
+  const client = await getClient();
+  const db = await getDB(client);
+  const myquery = { address: `Mountain 21` };
+  db.collection(`users`).deleteOne(myquery, (err) => {
+    if (err) throw err;
+    const myQueryJSON = JSON.stringify(myquery);
+    console.log(`1 document deleted ${myQueryJSON}`);
+  });
+  client.close();
+};
+
+async function dropCollection() {
+  const client = await getClient();
+  const db = await getDB(client);
+  db.collection(`users`).drop((err, delOK) => {
+    if (err) throw err;
+    if (delOK) console.log(`Collection deleted`);
+  });
+  client.close();
+};
+
+async function getDataFromJson() {
+  const client = await getClient();
+  const db = await getDB(client);
   db.collection(`users`).insertMany(users, (err) => {
-    if (err) { throw err; } else {
+    if (err) throw err;
+    else {
       console.log(users);
       console.log(`Data succesfully loaded from <users> collections`);
     }
   });
   db.collection(`cars`).insertMany(cars, (err) => {
-    if (err) { throw err; } else {
+    if (err) throw err;
+    else {
       console.log(cars);
       console.log(`Data succesfully loaded from <cars> collections`);
     }
   });
   client.close();
-}
+};
 
-async function getQuery () {
+async function getQuery() {
   const client = await getClient();
-  const db = client.db(`usersdb`);
+  const db = await getDB(client)
   const mysort = { name: 1 };
   const result = await db.collection(`users`).aggregate([
-    { $lookup:
+    {
+      $lookup:
       {
         from: `cars`,
         localField: `user_id`,
@@ -34,36 +69,36 @@ async function getQuery () {
         as: `cardetails`
       }
     },
-    { $replaceRoot: { newRoot: { $mergeObjects: [ { $arrayElemAt: [ `$cardetails`, 0 ] }, `$$ROOT` ] } } },
+    { $replaceRoot: { newRoot: { $mergeObjects: [{ $arrayElemAt: [`$cardetails`, 0] }, `$$ROOT`] } } },
     { $project: { cardetails: 0 } }
   ]).sort(mysort).toArray();
   client.close();
   return result;
-}
+};
 
-async function getClient () {
-  const url = `mongodb://localhost:27017`;
+async function getClient() {
+  const url = `mongodb://localhost:27017/`;
   const client = MongoClient.connect(url, { useNewUrlParser: true });
   console.log(`Successful server connection`);
   return client;
-}
+};
 
-async function getDB (client) {
+async function getDB(client) {
   const dbName = `usersdb`;
   const db = client.db(dbName);
   return db;
-}
+};
 
-async function getTitle () {
+async function getTitle() {
   let object = await getQuery();
   let arrayTitle = [];
   for (let key in object[0]) {
     arrayTitle.push(key);
   }
   return arrayTitle;
-}
+};
 
-async function getBody () {
+async function getBody() {
   let object = await getQuery();
   let arrayBody = [];
   object.forEach(element => {
@@ -74,16 +109,19 @@ async function getBody () {
     arrayBody.push(array);
   });
   return arrayBody;
-}
+};
 
-async function getData () {
+async function getData() {
   const body = await getBody();
   const title = await getTitle();
   body.unshift(title);
   return body;
-}
+};
 
 module.exports = {
+  findDocument,
+  deleteDocument,
+  dropCollection,
   getDataFromJson,
   getQuery,
   getData
